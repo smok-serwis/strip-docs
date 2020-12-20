4184be85ba531551f9693a3442fb35e0bbaeefba
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-12-20 18:29:08 +0100

    fix circleci

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 from strip_docs import __version__
 
 setup(keywords=['docs', 'stripping', 'pydoc', 'removal'],
-      packages=find_packages(include=['strip_docs']),
+      packages=find_packages(include=['strip_docs', 'strip_docs.*']),
       version=__version__,
       entry_points={
             "console_scripts": [
