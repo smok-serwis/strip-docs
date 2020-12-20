291c684f0e76c21ef43c419aa0fce71af7d8cb29
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-12-20 18:44:52 +0100

    fix circleci

--- tests/test_strip_docs.py
+++ tests/test_strip_docs.py
@@ -1,10 +1,11 @@
 import unittest
 import os
+from strip_docs.strip_docs import do_file
 
 
 class TestStripDocs(unittest.TestCase):
     def test_strip_docs(self):
-        self.assertFalse(os.system('sudo strip-docs tests'))
+        do_file('tests')
         with open('tests/file_to_strip.py', 'r') as f_in:
             d = f_in.read()
         self.assertNotIn('"""', d)
