d7ed62bcf7019c790d8e5f1aae405b04c2f86b91
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-12-20 19:00:28 +0100

    v1.0

--- strip_docs/run.py
+++ strip_docs/run.py
@@ -9,5 +9,5 @@ def run():
 strip-docs <name of directory with py, pyx and pyd files>
 ''')
         sys.exit(1)
-    for dir_or_file_name in sys.argv[1]:
+    for dir_or_file_name in sys.argv[1:]:
         do_path(dir_or_file_name)
